5b9a5cc3cf3f5e462153a6d7e79947ed3c873d90
cire.betoubam@ensea.fr <cire.betoubam@ensea.fr>
2024-01-25 13:48:27 +0100

    texte

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const listGames = [
 		title: "Clementine",
 		jeu: "The Walking Dead",
 		imageUrl:
-			"hhttps://static.wikia.nocookie.net/the-final-season-franchises/images/b/ba/Clementine_Marsh.jpg/revision/latest?cb=20201104064724",
+			"https://static.wikia.nocookie.net/the-final-season-franchises/images/b/ba/Clementine_Marsh.jpg/revision/latest?cb=20201104064724",
 	},
 	{
 		title: "V",
@@ -37,7 +37,7 @@ const listGames = [
 		title: "Chun-Li",
 		jeu: "Street Fighter",
 		imageUrl:
-			"https://www.reddit.com/media?url=https%3A%2F%2Fpreview.redd.it%2Fchun-li-art-by-akiman-based-on-the-upcoming-max-factory-v0-ia79r2r3oyja1.jpg%3Fwidth%3D640%26crop%3Dsmart%26auto%3Dwebp%26s%3D67d2d5aacdf85459017361ee362f1118eec4fb8b",
+			"https://i.pinimg.com/736x/ee/f6/75/eef6750a04d883f5f8b09fcea2993e1b.jpg",
 	},
 ]
 
