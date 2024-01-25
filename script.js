4b67d5ec04a5657f9bc05e62ab7f0844471c762f
cire.betoubam@ensea.fr <cire.betoubam@ensea.fr>
2024-01-25 13:41:51 +0100

    texte

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const listGames = [
 		title: "Chun-Li",
 		jeu: "Street Fighter",
 		imageUrl:
-			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUtb311YqWJkD6BO5ERORw9nZikI_b071lPA&usqp=CAU",
+			"https://www.reddit.com/media?url=https%3A%2F%2Fpreview.redd.it%2Fchun-li-art-by-akiman-based-on-the-upcoming-max-factory-v0-ia79r2r3oyja1.jpg%3Fwidth%3D640%26crop%3Dsmart%26auto%3Dwebp%26s%3D67d2d5aacdf85459017361ee362f1118eec4fb8b",
 	},
 ]
 
